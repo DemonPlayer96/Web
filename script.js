f913e5825b30e6501dee74730b9e8964b13aa3e6
DemonPlayer96 <diyari2006@gmail.com>
2025-02-20 11:19:36 +0300

    calculator app in javascript

---
+++ script.js
@@ -0,0 +1,40 @@
+const zeroBtn = document.getElementById("0");
+const oneBtn = document.getElementById("1");
+const twoBtn = document.getElementById("2");
+const threeBtn = document.getElementById("3");
+const fourBtn = document.getElementById("4");
+const fiveBtn = document.getElementById("5");
+const sixBtn = document.getElementById("6");
+const sevenBtn = document.getElementById("7");
+const eightBtn = document.getElementById("8");
+const nineBtn = document.getElementById("9");
+
+const showPlace = document.getElementById("show");
+
+function one() {
+    showPlace.textContent += "1";
+}
+function two() {
+    showPlace.textContent += "2";
+}
+function three() {
+    showPlace.textContent += "3";
+}
+function four() {
+    showPlace.textContent += "4";
+}
+function five() {
+    showPlace.textContent += "5";
+}
+function six() {
+    showPlace.textContent += "6";
+}
+function seven() {
+    showPlace.textContent += "7";
+}
+function eight() {
+    showPlace.textContent += "8";
+}
+function nine() {
+    showPlace.textContent += "9";
+}
