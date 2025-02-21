f6d259219cc999299ce75c444a01cd6ac7f4b9aa
DemonPlayer96 <diyari2006@gmail.com>
2025-02-21 15:06:05 +0300

    javascript calculator app

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const showPlace = document.getElementById("show");
+const showPast = document.getElementById("showPast");
 
 let firstNum = 0;
 let secondNum = 0;
@@ -41,32 +42,77 @@ function nine() {
     showPlace.textContent += "9";
 }
 function c() {
-    showPlace.textContent = Math.floor(showPlace.textContent / 10);
+    showPlace.textContent = showPlace.textContent.toString().split('').slice(0, -1).join('')
 }
 function ac() {
     firstNum = 0;
     secondNum = 0;
     operator = "";
-    result = 0;
     showPlace.textContent = "";
+    showPast.textContent = "";
 }
 function plus() {
     firstNum = showPlace.textContent;
     operator = "+";
+    showPast.textContent = firstNum;
     showPlace.textContent = "";
 }
 function minus() {
     firstNum = showPlace.textContent;
     operator = "-";
+    showPast.textContent = firstNum;
     showPlace.textContent = "";
 }
 function multiply() {
     firstNum = showPlace.textContent;
     operator = "*";
+    showPast.textContent = firstNum;
     showPlace.textContent = "";
 }
 function division() {
     firstNum = showPlace.textContent;
     operator = "/";
+    showPast.textContent = firstNum;
     showPlace.textContent = "";
+}
+function modules() {
+    firstNum = showPlace.textContent;
+    operator = "%";
+    showPast.textContent = firstNum;
+    showPlace.textContent = "";
+}
+function ans() {
+    showPlace.textContent = result;
+}
+function equal() {
+    if(operator == "+") {
+        secondNum = showPlace.textContent;
+        result = Number(firstNum) + Number(secondNum);
+        showPlace.textContent = result;
+        showPast.textContent = firstNum + " + " + secondNum + " = " + result;
+    }
+    else if(operator == "-") {
+        secondNum = showPlace.textContent;
+        result = Number(firstNum) - Number(secondNum);
+        showPlace.textContent = result;
+        showPast.textContent = firstNum + " - " + secondNum + " = " + result;
+    }
+    else if(operator == "*") {
+        secondNum = showPlace.textContent;
+        result = Number(firstNum) * Number(secondNum);
+        showPlace.textContent = result;
+        showPast.textContent = firstNum + " x " + secondNum + " = " + result;
+    }
+    else if(operator == "/") {
+        secondNum = showPlace.textContent;
+        result = Number(firstNum) / Number(secondNum);
+        showPlace.textContent = result;
+        showPast.textContent = firstNum + " / " + secondNum + " = " + result;
+    }
+    else if(operator == "%") {
+        secondNum = showPlace.textContent;
+        result = Number(firstNum) % Number(secondNum);
+        showPlace.textContent = result;
+        showPast.textContent = firstNum + " % " + secondNum + " = " + result;
+    }
 }
